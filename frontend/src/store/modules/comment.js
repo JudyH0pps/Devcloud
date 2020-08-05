c6e9c288b263e903e69e2a502872446f81a7f8c5
hjsong <song6240@naver.com>
2020-08-05 15:49:51 +0900

    UPDATE comment.js 댓글 수정,삭제 로직

--- frontend/src/store/modules/comment.js
+++ frontend/src/store/modules/comment.js
@@ -21,14 +21,31 @@ export default {
                 })
                 .catch(err => console.log(err))
         },
-        fetchComments(context, commentData) {
+        fetchComments(context, answer_id) {
             http
                 .get('/api/comments', {params: {
-                    answer_id: commentData.answer_id,
+                    answer_id: answer_id,
                 }})
                 .then(({data}) => {
                     console.log(data)
                     context.commit('setComments', data)
+                })
+                .catch(err => console.log(err))
+        },
+        editComments(context, commentData) {
+            http
+                .put('/api/comments', commentData)
+                .then(({data}) => {
+                    console.log(data)
+                    context.dispatch('fetchComments', commentData.answer_id)
+                })
+                .catch(err => console.log(err))
+        },
+        deleteComment(context, commentData) {
+            http
+                .delete('/api/comments', commentData.comment_id)
+                .then(() => {
+                    context.dispatch('fetchComments', commentData.answer_id)
                 })
                 .catch(err => console.log(err))
         }
