2999a50a8f07ce4d634e2dd7beea11f08b643471
moter2588 <moter2588@gmail.com>
2020-08-07 10:39:22 +0900

    ADD feature/board loading-overay add

--- frontend/src/views/Board.vue
+++ frontend/src/views/Board.vue
@@ -1,5 +1,20 @@
 <template>
     <div id="board">
+        <div class="vld-parent">
+            <loading :active.sync="isLoading" 
+            :can-cancel="true" 
+            :on-cancel="onCancel"
+            :is-full-page="fullPage"
+            color=#5ABEFF
+            :width="128"
+            :height="128"
+            >
+            </loading>
+            
+
+            <!--<label><input type="checkbox" v-model="fullPage">Full page?</label>-->
+            <!--<button @click.prevent="doAjax">fetch Data</button>-->
+        </div>
         <div class="container">
             <!-- 상단 글 작성 버튼 -->
             <!-- 로그인 후에 입력 가능 -->
@@ -9,9 +24,14 @@
                         default: "Top Questions"
                         Searched: "Search Results"
                     -->
-                    <div class="d-inline-block float-left">
+                    
+                    <div class="d-inline-block float-left" v-if="this.searchKeyword">
+                        <h2>Search Result: <span style="text-decoration: underline; color: gray; font-weight: none">{{this.searchKeyword}}</span></h2>
+                    </div>
+                    <div class="d-inline-block float-left" v-else>
                         <h2>All Questions</h2>
                     </div>
+                    
                     <div class="d-inline float-right">
                         <button @click="$router.push({name: 'WriteQuestion'})" class="btn btn-info">
                             Ask Question
@@ -37,6 +57,11 @@
 <script>
 import BoardResultlist from '../components/BoardResultlist.vue'
 import {mapState, mapActions} from 'vuex'
+
+// Import component
+import Loading from 'vue-loading-overlay';
+// Import stylesheet
+import 'vue-loading-overlay/dist/vue-loading.css';
 export default {
     computed:{
         ...mapState({
@@ -49,16 +74,31 @@ export default {
     name: 'Board',
     components: {
         BoardResultlist,
+        Loading,
     },
     data(){
         return {
+            isLoading: false,
+            fullPage: true
         }
     },
     methods:{
         ...mapActions('question',['fetchQuestionsByKeyword']),
+
+        doAjax() {
+            this.isLoading = true;
+            // simulate AJAX
+            setTimeout(() => {
+                this.isLoading = false
+            },800)
+        },
+        onCancel() {
+            console.log('User cancelled the loader.')
+        },
     },
     created(){
-        this.fetchQuestionsByKeyword(this.searchKeyword)
+        this.fetchQuestionsByKeyword(this.searchKeyword),
+        this.doAjax()
     },
     watch:{
         searchKeyword : function(){
