dd972e3190990fd386de488b7ea6413b37218c06
hjsong <song6240@naver.com>
2020-08-13 11:41:32 +0900

    ADD 댓글 작성란

--- desgin/src/components/DetailAnswer.vue
+++ desgin/src/components/DetailAnswer.vue
@@ -11,29 +11,71 @@
             </div>
         </div>
         <!-- <p class="q-content">{{ answer.content }}</p> -->
-        <div v-html="answer.content" class="content-reader"></div>
-        <span style="float: right; margin: 0 2px; cursor: pointer" v-if="parseInt($cookie.get('user_id')) === answer.user.id" @click="deleteAnswer({ answer_id: answer.id, question_id: $route.params.question_id })">삭제</span>
-        <span @click="$router.push({ name : 'EditAnswer', params : { question_id : $route.params.question_id, answer_id: answer.id } })" style="float: right; margin: 0 2px; cursor: pointer;" v-if="parseInt($cookie.get('user_id')) === answer.user.id">수정</span>
+        <div v-html="answer.content" class="q-content"></div>
+        <div class="q-footer">
+            <span style="cursor: pointer" @click="changeCommentInput">댓글 달기</span>
+            <div>
+                <span style="float: right; margin: 0 2px; cursor: pointer" v-if="parseInt($cookie.get('user_id')) === answer.user.id" @click="deleteAnswer({ answer_id: answer.id, question_id: $route.params.question_id })">삭제</span>
+                <span @click="$router.push({ name : 'EditAnswer', params : { question_id : $route.params.question_id, answer_id: answer.id } })" style="float: right; margin: 0 2px; cursor: pointer;" v-if="parseInt($cookie.get('user_id')) === answer.user.id">수정</span>
+            </div>
+        </div>
+
+        <!-- 댓글 작성란 -->
+        <div v-show="commentInput">
+            <input @keyup.enter="postComment({user_id: parseInt($cookie.get('user_id')), answer_id: answer.id,content: postContent})" v-model="postContent" type="text" placeholder="악의가 담긴 댓글은 누군가를 상처입힐 수 있습니다.">
+            <button @click="postComment({user_id: parseInt($cookie.get('user_id')), answer_id: answer.id,content: postContent})">작성</button>
+		</div>
+
+        <!-- 댓글 목록 -->
+        <ul v-if="comments">
+			<!-- <li v-for="comment in comments" :key="comment.id">
+				<img src="" alt="user_profile">
+				<div>
+					<header>
+						<h5>{{ comment.user.name }}</h5>
+						<span>{{ comment.create_at }}</span>
+					</header>
+					<div>
+						<p>{{ comment.content }}</p>
+						<div>
+							<button>Edit</button>
+							<button>Delete</button>
+						</div>
+					</div>
+				</div>
+			</li> -->
+		</ul> 
     </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import { mapState, mapActions } from 'vuex'
 export default {
     name: 'DetailAnswer',
     data() {
         return {
+            commentInput: false,
+            postContent: ''
         }
     },
     props: {
         answer: Object,
     },
+    computed: {
+		...mapState({
+			comments: state => state.comment.comments
+		}),
+	},
     methods: {
-        ...mapActions('answer', ['deleteAnswer'])
+        ...mapActions('answer', ['deleteAnswer']),
+        ...mapActions('comment',['postComment', 'fetchComments', 'editComment', 'deleteComment']),
+        changeCommentInput() {
+			this.commentInput = !this.commentInput
+		}
+    },
+    created() {
+		this.fetchComments(this.answer.id)
     }
-    // created() {
-    //     console.log(this.answer);
-    // }
 }
 </script>
 
@@ -75,5 +117,11 @@ export default {
 .content-reader >>> .ql-syntax {
     background-color: black;
     color: white;
+    padding: 3px 5px;
+    border-radius: 3px;
+}
+.q-footer {
+    display: flex; 
+    justify-content: space-between;
 }
 </style>
