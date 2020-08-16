342ff2e5a3f0c29748267ef5a0f05a8e682176a2
tlsdorye <tlsdorye@gmail.com>
2020-08-16 09:23:14 +0900

    update rank api

--- backend/src/main/java/com/ssafy/blog/controller/RankController.java
+++ backend/src/main/java/com/ssafy/blog/controller/RankController.java
@@ -1,5 +1,6 @@
 package com.ssafy.blog.controller;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
@@ -7,15 +8,15 @@ import java.util.List;
 import java.util.Optional;
 
 import com.ssafy.blog.model.Rank;
-import com.ssafy.blog.model.RankRecord;
-import com.ssafy.blog.repository.RankRecordRepository;
+import com.ssafy.blog.payload.rank.RankResponse;
 import com.ssafy.blog.repository.RankRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.ApiOperation;
@@ -26,73 +27,62 @@ public class RankController {
     @Autowired
     private RankRepository rankRepository;
 
-    @Autowired
-    private RankRecordRepository rankRecordRepository;
-
     @GetMapping("/api/rank")
     @ApiOperation(value = "랭크 조회")
-    public ResponseEntity<Rank> searchRank(@RequestParam("user_id") Long user_id) {
-        Optional<Rank> optionalRank = rankRepository.findByUserId(user_id);
-        if (!optionalRank.isPresent())
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        return new ResponseEntity<>(optionalRank.get(), HttpStatus.OK);
-    }
+    public ResponseEntity<List<RankResponse>> searchRank() {
+        List<RankResponse> list = new ArrayList<>();
+        List<Rank> rlist = rankRepository.findAll();
 
-    @GetMapping("/api/rankupdate")
-    @ApiOperation(value = "랭크 업데이트")
-    public ResponseEntity<String> updateRank() {
-        List<Rank> rankList = rankRepository.findAll();
-        for (Rank rank : rankList) {
-            rank.setRankPoint(
-                    rank.getQuestionCnt() + rank.getAnswerCnt() + rank.getLikeCnt() + rank.getSelectCnt() * 10);
-        }
-        Collections.sort(rankList, new Comparator<Rank>() {
+        for(Rank rank : rlist) calculateRanking(rank);
+        Collections.sort(rlist, new Comparator<Rank>(){
             @Override
             public int compare(Rank o1, Rank o2) {
                 return o2.getRankPoint() - o1.getRankPoint();
             }
         });
 
-        for (int i = 0; i < rankList.size(); i++) {
-            Rank rank = rankList.get(i);
+        for(int i=0;i<rlist.size();i++) {
+            Rank rank = rlist.get(i);
             rank.setRanking(i + 1);
             rankRepository.save(rank);
+            list.add(makeResponse(rank));
         }
 
-        return new ResponseEntity<>("success", HttpStatus.OK);
-    }
-
-    @GetMapping("/api/rank/record")
-    @ApiOperation(value = "랭크 기록 조회")
-    public ResponseEntity<List<RankRecord>> searchRankRecord(
-            @RequestParam(required = false, name = "user_id") Long user_id) {
-        List<RankRecord> list = null;
-        
-        if(user_id != null) {
-            list = rankRecordRepository.findByUserId(user_id);
-
-        } else {
-            list = rankRecordRepository.findAll();
-
-        }
         return new ResponseEntity<>(list, HttpStatus.OK);
     }
 
-    @GetMapping("/api/rank/recordadd")
-    @ApiOperation(value = "랭크 기록 추가")
-    public ResponseEntity<String> addRankRecord() {
-        Date now = new Date();
-        List<Rank> rankList = rankRepository.findAll();
-        for(Rank rank : rankList) {
-            RankRecord rankRecord = new RankRecord();
-            rankRecord.setUser(rank.getUser());
-            rankRecord.setRankPoint(rank.getRankPoint());
-            rankRecord.setRanking(rank.getRanking());
-            rankRecord.setCreatedAt(now);
-            
-            rankRecordRepository.save(rankRecord);
-        }
+    @GetMapping("/api/rank/{user_id}")
+    @ApiOperation(value = "유저 아이디로 랭크 조회")
+    public ResponseEntity<RankResponse> searchRankByUserId(@PathVariable("user_id") Long user_id) {
+        Optional<Rank> optionalRank = rankRepository.findByUserId(user_id);
+        if (!optionalRank.isPresent())
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        RankResponse response = makeResponse(optionalRank.get());
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
 
-        return new ResponseEntity<>("success", HttpStatus.OK);
+
+    private void calculateRanking(Rank rank) {
+        rank.setRankPoint(
+            rank.getQuestionCnt() +
+            rank.getAnswerCnt() + 
+            rank.getLikeCnt() + 
+            rank.getSelectCnt() * 10
+        );
+        rank.setUpdatedAt(new Date());
+    }
+
+    private RankResponse makeResponse(Rank rank) {
+        RankResponse res = new RankResponse();
+        res.setRank_id(rank.getId());
+        res.setUser_id(rank.getUser().getId());
+        res.setRank_point(rank.getRankPoint());
+        res.setRanking(rank.getRanking());
+        res.setQuestion_cnt(rank.getQuestionCnt());
+        res.setAnswer_cnt(rank.getAnswerCnt());
+        res.setLike_cnt(rank.getLikeCnt());
+        res.setSelect_cnt(rank.getSelectCnt());
+        res.setUpdated_at(rank.getUpdatedAt());
+        return res;
     }
 }
