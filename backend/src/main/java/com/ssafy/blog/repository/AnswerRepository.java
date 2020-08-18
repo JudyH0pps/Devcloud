6bf864668733ae90a6eca5c812ca8a2143be1e03
tlsdorye <tlsdorye@gmail.com>
2020-08-18 14:26:31 +0900

    fix api bug

--- backend/src/main/java/com/ssafy/blog/repository/AnswerRepository.java
+++ backend/src/main/java/com/ssafy/blog/repository/AnswerRepository.java
@@ -5,13 +5,14 @@ import java.util.Optional;
 
 import com.ssafy.blog.model.Answer;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
 
-    List<Answer>findAllByQuestionId(Long question_id);
+    List<Answer>findAllByQuestionId(Long question_id, Sort sort);
 
     List<Answer> findAllByUserId(Long user_id);
 
