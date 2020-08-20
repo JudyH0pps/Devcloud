263dbbb46493367a4b8de726bce0db4322aac045
신승현 <tlsdorye@gmail.com>
2020-08-20 14:05:06 +0900

    change server url

--- desgin/src/util/http-common.js
+++ desgin/src/util/http-common.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // axios 객체 생성
 export default axios.create({
 //   baseURL: 'http://localhost:9999/happyhouse/api',
-  baseURL: 'http://i3c202.p.ssafy.io:8080',
+  baseURL: 'http://i3c202.p.ssafy.io',
   headers: {
     'Content-type': 'application/json',
   },
